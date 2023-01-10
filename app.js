12ebf3bb31dd2b1302caec919cf141e98113cc2e
Suvadeep Maity <maityamalendu18@gmailcom>
2023-01-10 23:41:57 +0530

    add ejs and change index.js file

--- app.js
+++ app.js
@@ -1,20 +1,25 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require("express");
+const app = express();
+const port = 3000;
 
-app.set("view engine","ejs")
+app.set("view engine", "ejs");
 
-app.get("/",(req,res)=>{
-    res.render("index")
-})
+app.get("/", (req, res) => {
+  const items = [
+    { name: "name1", important: "Yes" },
+    { name: "name2", important: "No" },
+    { name: "name3", important: "Yes" },
+  ];
+  res.render("index", { items });
+});
 
-app.get("/add-item",(req,res)=>{
-    res.render("add-item")
-})
-app.use((req,res)=>{
-    res.render("error")
-})
+app.get("/add-item", (req, res) => {
+  res.render("add-item");
+});
+app.use((req, res) => {
+  res.render("error");
+});
 
-app.listen(port,()=>{
-    console.log(`Server is running on port ${port}`);
-})
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
